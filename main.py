1501826d070f50951460407165a7fcb8b4488ede
Elias Pereira <elias.pereira@farme.com.br>
2024-12-23 11:10:50 -0300

    temos um novo endpoint no metodo POST, que temos como função de dicionar um novo curso ao dicionário cursos e ele utiliza modelo Curso para validar os dados recebidos na requisição e cria automaticamente um novo ID para o curso.

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-from fastapi import FastAPI, HTTPException, status
+from typing import List, Optional
+from fastapi import FastAPI
+from fastapi import HTTPException
+from fastapi import status
+
+from models import Curso
+
 
 app = FastAPI()
 
@@ -15,6 +21,7 @@ cursos = {
     }
 }
 
+
 @app.get('/cursos')
 async def get_cursos():
     return cursos
@@ -27,6 +34,15 @@ async def get_curso(curso_id: int):
     except KeyError:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Curso não encontrado.') 
 
+@app.post('/cursos', status_code=status.HTTP_201_CREATED)
+async def post_curso(curso: Curso):
+    next_id: int = len (curso) + 1
+    cursos[next_id] = curso
+    del curso.id
+    return curso
+
+
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("main:app", host="0.0.0.0", port=8000, log_level="info", reload=True)
+    
